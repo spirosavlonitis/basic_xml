5bdae1c33fed241dadc56f0947ff7663f83451a1
Spiros Avlonitis <spirosa84@hotmail.com>
2019-02-28 04:57:09 +0200

    target output

--- basic_xml.rb
+++ basic_xml.rb
@@ -1,25 +1,27 @@
 class BasicXml < BasicObject
 
-	def initialize indent:, target:
+	def initialize indent:, target:, header: true
 		@indent = indent
 		@cur_indent = 0
 		@target = target
+		@target.puts '<?xml version="1.0" encoding="UTF-8"?>' if header
 	end
 
 	def method_missing tag, *args, &block
-
-		if args.size == 0			# only a tag close if no block is given
-			@target.puts "#{' ' * @cur_indent}<#{block ? '' : '/' }#{tag}>"
-		elsif args[0].is_a? ::String and block		# malformed xml tag
-			::Object.send(:raise, ::ArgumentError, "Cannot mix a text argument with a block")
-		elsif args[0].is_a? ::String  # tag content with or without attributes
-				@target.print "#{' ' * @cur_indent}<#{tag} "
-				if args.size == 2	  # with attributes
-					args[1].each do |attrb, value|
-						@target.print %Q[#{attrb}="#{value}"#{' ' unless attrb == args[1].keys.last}]
-					end
+		if args[0].is_a? ::String and block		# malformed xml tag
+			::Object.send(:raise, ::ArgumentError, "Cannot mix a text argument with a block") 
+		end
+		
+		if args.size == 0								# only a tag
+			@target.puts "#{' ' * @cur_indent}<#{block ? '' : '/' }#{tag}>"	# close tag if no block
+		elsif args[0].is_a? ::String					# tag content with or without attributes
+			@target.print "#{' ' * @cur_indent}<#{tag} "
+			if args.size == 2	  						# with attributes
+				args[1].each do |attrb, value|
+					@target.print %Q[#{attrb}="#{value}"#{' ' unless attrb == args[1].keys.last}]
 				end
-				@target.puts ">#{args[0]}</#{tag}>"
+			end
+			@target.puts ">#{args[0]}</#{tag}>"
 		else
 			@target.print "#{' ' * @cur_indent}<#{tag} "
 			args[0].each do |attrb, value| 
@@ -37,11 +39,12 @@ class BasicXml < BasicObject
 	end
 end
 
-xml = BasicXml.new indent: 2, target: STDOUT
+fp = File.open("test.xml", "w")
+
+xml = BasicXml.new indent: 2, target: fp
 
 xml.foo "Matz", language: "Ruby", year: 1992
 xml.buz coder: "John Doe"
-
 xml.foo(language: "Ruby", year: 1996) {
 	xml.bar
 }
@@ -52,3 +55,6 @@ xml.foo {
 		xml.foobar "Name"
 	}
 }
+
+
+fp.close
