eecef49818327802f0b9227f7d95331621b7cda3
Spiros Avlonitis <spirosa84@hotmail.com>
2019-02-28 03:35:50 +0200

    foo

--- basic_xml.rb
+++ basic_xml.rb
@@ -8,41 +8,51 @@ class BasicXml < BasicObject
 
 	def method_missing tag, *args, &block
 
-		if args.size == 0
+		if args.size == 0			# only a tag close if no block is given
 			@target.puts "#{' ' * @cur_indent}<#{block ? '' : '/' }#{tag}>"
-		elsif args[0].is_a? ::String and block
-			::Object.send(:raise, ::ArgumentError, "cannot mix a text argument with a block")
-		else
-			if args[0].is_a? ::String and args.size == 1
-				@target.puts "#{' ' * @cur_indent}<#{tag}>#{args[0]}</#{tag}>"
-			elsif args[0].is_a? ::String
-				@target.print "#{' ' * @cur_indent}<#{tag} "
-				args[1].each do |attrb, value| 
-					@target.print %Q[#{attrb}="#{value}"#{' ' unless attrb == args[1].keys.last}]
+		elsif args[0].is_a? ::String and block		# malformed xml tag
+			::Object.send(:raise, ::ArgumentError, "Cannot mix a text argument with a block")
+		else				# check if tag content, attribute or both are given
+			if args[0].is_a? ::String
+				if args.size == 1
+					@target.puts "#{' ' * @cur_indent}<#{tag}>#{args[0]}</#{tag}>"
+				else
+					@target.print "#{' ' * @cur_indent}<#{tag} "
+					args[1].each do |attrb, value| 
+						@target.print %Q[#{attrb}="#{value}"#{' ' unless attrb == args[1].keys.last}]
+					end
+					@target.puts ">#{args[0]}</#{tag}>"
 				end
-
-				@target.puts ">#{args[0]}</#{tag}>"
+			else
+				@target.print "#{' ' * @cur_indent}<#{tag} "
+				args[0].each do |attrb, value| 
+					@target.print %Q[#{attrb}="#{value}"#{' ' unless attrb == args[0].keys.last}]
+				end
+				@target.puts  block ? ">" : "/>"
 			end
 		end
 
-		if block
+		if block				# add indent when called with a block
 			@cur_indent += @indent
 			block.call()
 			@cur_indent -= @indent
 			@target.puts "#{' ' * @cur_indent}</#{tag}>"
 		end
-
 	end
 end
 
 xml = BasicXml.new indent: 2, target: STDOUT
 
-
 xml.foo "Matz", language: "Ruby", year: 1992
+xml.buz coder: "John Doe"
+
+xml.foo(language: "Ruby", year: 1996) {
+	xml.bar
+}
 
 xml.foo {
 	xml.bar {
-		xml.buz
+		xml.buz(coder: "John Doe") { xml.foobarbuzz "Name", dude: 'Hey' }
 		xml.foobar "Name"
 	}
 }
